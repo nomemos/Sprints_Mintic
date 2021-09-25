c60e511ea2ecbc9e80846bf50c9bb9df372f0ceb
Oscar Ayala <ayalaoscar957@gmail.com>
2021-09-24 21:22:30 -0500

    archivos modificados

---
+++ main.js
@@ -0,0 +1,8 @@
+$('.checkbox').click(function(){
+    if ($('input.checkbox').is(':checked')){
+            $('.theme').attr('href', 'dark.css');
+    }
+    else{
+        $('.theme').attr('href', 'light.css');
+    }
+});

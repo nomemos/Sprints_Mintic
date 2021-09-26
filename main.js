a2e8ec6744954a7ff9cad5554ebcdb7003add4e9
OSCARCHAMUCERO <oscarchamucero@hotmail.com>
2021-09-25 23:54:57 -0500

    actualizaciòn tamaño de letra

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
+
 $('.checkbox').click(function(){
     if ($('input.checkbox').is(':checked')){
-            $('.theme').attr('href', 'dark.css');
+    $('.theme').attr('href', 'dark.css');
     }
     else{
-        $('.theme').attr('href', 'light.css');
+    $('.theme').attr('href', 'light.css');
     }
-});
+    });
+    
+    var cambio =document.getElementById('cambio'),
+    parrafo = document.getElementById('parrafo'),
+    contador=0;
+
+    function cambiar()
+    { if (contador==0)
+        { 
+            parrafo.classList.add('ampliado');
+                contador=1;
+        }
+
+        else 
+        {
+            parrafo.classList.remove('ampliado');
+            contador=0;
+        }
+    }
+cambio.addEventListener('click',cambiar,true)
